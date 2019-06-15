c5d7b07f7fd86dca0c439d70a94b30875f2a84a6
Sanchit Sahay <sanchitsahay.tech@gmail.com>
2019-06-15 16:10:27 +0530

    fixing scopes and stuff

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 import os
 from mutagen.easyid3 import EasyID3
 
-acoustid_key = '' #enter your acoustid api key here
-lastfm_key = '' #enter you lastfm api key here
+#acoustid_key = '' #enter your acoustid api key here
+#lastfm_key = '' #enter you lastfm api key here
 
 class Error(Exception):
 	pass
@@ -64,7 +64,7 @@ def apply_metadata(data, filename):
 	audio.save()
 	os.rename(filename, title + '.mp3')
 
-def main_process(file):
+def main_process(file, acoustid_key, lastfm_key):
 	print("processing...", end="")
 	try:
 		arty, name = (fetch_aid_data(match_fingerprint(gen_fingerprint(file), acoustid_key)))
@@ -77,6 +77,8 @@ def main_process(file):
 		print("error(audio fingerprint not found in AcoustID)")
 
 def main():
+	acoustid_key = ''
+	lastfm_key = ''
 	if not acoustid_key:
 		acoustid_key = input("Enter your AcoustID API key: ")
 	if not lastfm_key:
@@ -86,7 +88,7 @@ def main():
 	for file in files:
 		if file[-4::] == '.mp3':
 			print(file, end="- ")
-			main_process(file)
+			main_process(file, acoustid_key, lastfm_key)
 		else:
 			continue
 
